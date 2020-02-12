bebe9c77f8ec599f0a453f4107c67a8108bf8f89
Sasha <sashaberduchev@gmail.com>
2020-02-12 21:52:59 +0200

    InternetMarket 5.6.2

--- InternetMarket/InternetMarket/InternetMarketEDM.edmx.sql
+++ InternetMarket/InternetMarket/InternetMarketEDM.edmx.sql
@@ -2,8 +2,8 @@
 -- --------------------------------------------------
 -- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
 -- --------------------------------------------------
--- Date Created: 01/24/2020 20:25:27
--- Generated from EDMX file: C:\Все Мои данные\Проекты\C-Sharp\InternetMarketProject\InternetMarket\InternetMarket\InternetMarketEDM.edmx
+-- Date Created: 02/12/2020 17:24:54
+-- Generated from EDMX file: D:\Projects\C#\InternetMarketProject\InternetMarket\InternetMarket\InternetMarketEDM.edmx
 -- --------------------------------------------------
 
 SET QUOTED_IDENTIFIER OFF;
